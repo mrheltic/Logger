c9461038551375b68cdec52b62e5890787ed9616
Vincenzo Pio Florio <vincenzopioflorio@gmail.com>
2023-10-24 11:56:43 +0200

    Init
    Added:
    
    -Initialized platform.ini
    -Blinking test
    -Declared variables in main

---
+++ src/blinking_test.cpp
@@ -0,0 +1,27 @@
+/*
+
+Run this code if you need to test the board, comment the main code and uncomment this one
+
+*/
+
+/*#include <Arduino.h>
+
+#define PIN_LED1 12
+#define PIN_LED2 13
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Hello ESP32C3!!");
+  pinMode(PIN_LED1, OUTPUT);
+  pinMode(PIN_LED2, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(PIN_LED1, HIGH);
+  digitalWrite(PIN_LED2, LOW);
+  delay(200);
+  digitalWrite(PIN_LED2, HIGH);
+  digitalWrite(PIN_LED1, LOW);
+  delay(200);
+}
+*/
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,68 @@
 #include <Arduino.h>
+//#include <SD.h>
+#include <FS.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define OLED_RESET 4
 
-// put function declarations here:
-int myFunction(int, int);
+//DECLARING VARIABLES FOR SCREEN
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+//DECLARING VARIABLES FOR BUTTONS
+#define BUTTON_1 12
+#define BUTTON_2 18
+#define BUTTON_3 19
+
+//DECLARING VARIABLES FOR SD CARD
+#define SD_CS 7
+
+
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  //INITIALIZING SCREEN
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;  // Don't proceed, loop forever
+  }
+
+  display.clearDisplay();
+
+  //INITIALIZING BUTTONS
+  pinMode(BUTTON_1, INPUT);
+  pinMode(BUTTON_2, INPUT);
+  pinMode(BUTTON_3, INPUT);
+
+  //INITIALIZING SD CARD
+  /*if(!SD.begin(SD_CS)) {
+    Serial.println(F("SD card failed"));
+    for(;;);
+  }
+  */
+
+  //DISPLAYING TEXT ON SCREEN
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Fattuale");
+  display.display();
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
