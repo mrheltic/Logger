8bed9cced705cfa3ca063177fa1f5d2c86aa88d2
Vincenzo Pio Florio <vincenzopioflorio@gmail.com>
2023-12-22 16:39:45 +0100

    Implemented new model logic

--- src/main.cpp
+++ src/main.cpp
@@ -5,109 +5,145 @@ int i = 0;
 class Measurement
 {
 private:
-  int *measurements;       // Array delle misurazioni
-  int length;              // Lunghezza dell'array
-  int count;               // Numero di elementi attualmente presenti nell'array
-  float mean;              // Media
-  float std;               // Deviazione standard
-  unsigned long timestamp; // Timestamp della prima misurazione
+    int *measurements;       // Array delle misurazioni
+    int length;              // Lunghezza dell'array
+    int count;               // Numero di elementi attualmente presenti nell'array
+    float mean;              // Media
+    float std; 
+    float sum;              // Deviazione standard
+    unsigned long timestamp; // Timestamp della prima misurazione
+    bool arrayFull;
 
 public:
-  Measurement(int len)
-  {
+    Measurement(int len);
+    ~Measurement();
+    void setLength(int len);
+    void insertMeasurement(int value);
+    void calculateMean();
+    void calculateStd();
+    void reset();
+    void setTimestamp(unsigned long timestamp);
+    float getMean();
+    float getStd();
+    int *getMeasurements();
+    bool isArrayFull();
+    int getLength();
+    void setArrayFull(bool arrayFull);
+    int getArrayLenght();
+};
+
+Measurement::Measurement(int len)
+{
     length = len;
     measurements = new int[length];
     count = 0;
     mean = 0.0;
     std = 0.0;
+    sum = 0;
     timestamp = 0;
-  }
+}
 
-  ~Measurement()
-  {
+Measurement::~Measurement()
+{
     delete[] measurements;
-  }
+}
 
-  void setLength(int len)
-  {
+void Measurement::setLength(int len)
+{
     delete[] measurements;
     length = len;
     measurements = new int[length];
     count = 0;
     mean = 0.0;
     std = 0.0;
+    sum = 0;
     timestamp = 0;
-  }
+}
 
-  void calculateMean()
-  {
-    int sum = 0;
-    for (int i = 0; i < length; i++)
-    {
-      sum += measurements[i];
-    }
+// We preferred to calculate the mean in this way because we don't need to re-iterate the array when calculate the mean (so other 860 iterations in)
+void Measurement::calculateMean()
+{
     mean = static_cast<float>(sum) / length;
-  }
+}
 
-  void calculateStd()
-  {
+void Measurement::calculateStd()
+{
     float sum = 0.0;
     for (int i = 0; i < length; i++)
     {
-      sum += pow(measurements[i] - mean, 2);
+        sum += pow(measurements[i] - mean, 2);
     }
     std = sqrt(sum / length);
-  }
+    sum = 0;
+}
 
-  bool isArrayFull()
-  {
-    return count == length;
-  }
+bool Measurement::isArrayFull()
+{
+    return arrayFull;
+}
 
-  void insertMeasurement(int value)
-  {
-    if (count < length)
+void Measurement::insertMeasurement(int value)
+{
+    measurements[count] = value;
+    count++;
+
+    if(count == length)
     {
-      measurements[count] = value;
-      count++;
+        count = 0;
+        arrayFull = true;
+        calculateMean();
+        calculateStd();
+        sum = 0;
     }
-    if (isArrayFull())
-    {
-      calculateMean();
-      calculateStd();
-    }
-  }
 
-  void clearMeasurements()
-  {
+    sum += value;
+}
+
+void Measurement::reset()
+{
+    length = 0;
     count = 0;
     mean = 0.0;
     std = 0.0;
-    memset(measurements, 0, sizeof(measurements));
-  }
+    timestamp = 0;
+    arrayFull = false;
+}
 
-  float getMean()
-  {
+float Measurement::getMean()
+{
     return mean;
-  }
+}
 
-  float getStd()
-  {
+float Measurement::getStd()
+{
     return std;
-  }
+}
 
-  int *getMeasurements()
-  {
-    return measurements;
-  }
+int *Measurement::getMeasurements()
+{
+    return this->measurements;
+}
 
-  void setTimestamp(unsigned long timestamp)
-  {
+void Measurement::setTimestamp(unsigned long timestamp)
+{
     this->timestamp = timestamp;
-  }
-};
+}
 
-Measurement m(860);
+int Measurement::getLength()
+{
+    return length;
+}
+
+void Measurement::setArrayFull(bool arrayFull)
+{
+    this->arrayFull = arrayFull;
+}
+
+int Measurement::getArrayLenght(){
+    return sizeof(measurements)/sizeof(measurements[0]);
+}
+
+Measurement measurement(860);
 unsigned long time_now;
 unsigned long time_old;
 
@@ -118,24 +154,19 @@ void setup()
 
 void loop()
 {
-
-  m.insertMeasurement(random(0, 1024));
-  i++;
-  delay(1);
-
-  if (m.isArrayFull())
-  {
-    Serial.println("Mean: " + String(m.getMean()));
-    Serial.println("Std: " + String(m.getStd()));
-    Serial.write((const uint8_t*)m.getMeasurements(), 860);
-
-    i = 0;
-    m.clearMeasurements();
-    Serial.println("Cleared");
-
-    time_now = millis();
-    Serial.print("ci ho messo: ");
-    Serial.println(time_now - time_old + 140);
-    time_old = time_now;
-  }
+  if (!measurement.isArrayFull())
+        {
+            // Serial.println(ads.getLastConversionResults());
+            measurement.insertMeasurement(random(0, 1000));
+        }
+        else
+        {
+            Serial.write(measurement.getMeasurements(), 860);
+            Serial.println("Mean: " + String(measurement.getMean()));
+            Serial.println("------------------------------------------------------------------------------");
+            // Serial.println("Std: " + String(measurement.getStd()));
+            Serial.println("Array full, resetted");
+            Serial.println("Array length: " + String(measurement.getLength()) + "\n");
+            measurement.setArrayFull(false);
+        }
 }
